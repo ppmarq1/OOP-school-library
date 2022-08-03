0233a5a20c1347d8bffa6a699ed923a7f0b9bafd
Pedro Marques <ppmarq1@gmail.com>
2022-08-03 16:51:03 +0100

    Fix linters

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,11 +1,12 @@
 require './capitalize_decorator'
 require './person'
+
 describe CapitalizeDecorator do
   before :each do
     @person = Person.new 43, 'silvamarques'
     @capitalize = CapitalizeDecorator.new(@person)
   end
-  it 'should CAPS person name' do
+  it 'it should CAPS person name' do
     expect(@capitalize.correct_name).to eq 'SILVAMARQUES'
   end
 end
