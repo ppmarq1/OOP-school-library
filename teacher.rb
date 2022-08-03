45785aed2c4710d31d225634a91305233399b495
Pedro Marques <ppmarq1@gmail.com>
2022-08-03 13:25:47 +0100

    Fix linters errors

--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,15 @@
-require_relative './person'
+# rubocop: disable Style/OptionalBooleanParameter
+require './person'
 
 class Teacher < Person
-  def initialize(specialization, age, name = 'Unknown', parent_permission: true)
-    super(age, parent_permission: parent_permission)
+  def initialize(specialization, age, name = 'Unknown', parent_permission = true)
+    super(age, name, parent_permission)
     @specialization = specialization
-    @name = name
   end
 
   def can_use_services?
     true
   end
 end
+
+# rubocop: enable Style/OptionalBooleanParameter
