985e0b61492e2fb16641e2e1583a0dc1cf8c8af2
AlaaAlsalem <alsalem.aladdin@gmail.com>
2022-08-03 18:17:35 +0300

    Make a method to test user class

---
+++ data/capitalize_decorator_spec.rb
@@ -0,0 +1,11 @@
+require './capitalize_decorator'
+require './person'
+describe CapitalizeDecorator do
+  before :each do
+    @person = Person.new 43, 'silvamarques'
+    @capitalize = CapitalizeDecorator.new(@person)
+  end
+  it 'should CAPS person name' do
+    expect(@capitalize.correct_name).to eq 'SILVAMARQUES'
+  end
+end 
